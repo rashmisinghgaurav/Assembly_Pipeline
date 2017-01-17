be2d1ce95f3ac107adedd1f69d0353d24eff465e
ashmkapo <ashmkapo>
2017-01-17 18:46:43 +0530

    Adding jenkinsdemo

---
+++ cookbooks/jenkinsdemo/README.md
@@ -0,0 +1,4 @@
+# jenkinsdemo
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/jenkinsdemo/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: jenkinsdemo
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
